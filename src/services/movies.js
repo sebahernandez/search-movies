5e55cc71d18bcf3d0fe6df839955a65b44682b38
Caracolazza <120380236+Caracolazza@users.noreply.github.com>
2023-02-25 21:34:31 -0300

    fix url

--- src/services/movies.js
+++ src/services/movies.js
@@ -2,7 +2,7 @@ const API_KEY = '27c23ee7' ;
 export const searchMovies = async ({search}) =>{
     if (search === '') return null;
     try{
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
         const json = await response.json();
         
         const movies = json.Search;
