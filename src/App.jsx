6f81aa209c2221c3566a290f35f141010be0fe9f
Caracolazza <120380236+Caracolazza@users.noreply.github.com>
2023-05-18 13:30:01 -0400

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState, useRef, useCallback} from 'react';
 import {useMovies} from './hooks/useMovies';
 import {Movies} from './components/Movies';
 import debounce from 'just-debounce-it';
+import Footer from './components/Footer';
 
 function useSearch(){
   const [error, setError] = useState(null);
@@ -42,12 +43,12 @@ function App() {
   const {search, updateSearch, error} = useSearch();
   const {movies, loading, getMovies} = useMovies({search, sort});
 
-
-  const debouncedGetMovies = useCallback(debounce(search => {
+// Busca las peliculas al escribir en el campo input con onchange
+  /* const debouncedGetMovies = useCallback(debounce(search => {
     getMovies({search})
   },300)
   ,[getMovies]
-  )
+  ) */
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -61,60 +62,59 @@ function App() {
   const handleChange = (e) => {
     const newSearch = e.target.value;
     updateSearch(newSearch)
-    debouncedGetMovies(newSearch);
+    /* debouncedGetMovies(newSearch); */
   }
 
   return (
-  <div className="container mx-auto flex flex-col justify-center items-center">
-      <h1 className="text-center text-5xl font-bold text-blue-500 py-9">Finder Film</h1>
-      <form className="w-3/5 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-      <div className="flex">
-          <input 
-            onChange={handleChange} 
-            value={search} 
-            name="query" 
-            placeholder='Avengers, Star Wars, Matrix...' 
-            className='flex-grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-          />
-          <button className='ml-2
-            bg-blue-500 
-            hover:bg-blue-400 
-            text-white 
-            font-bold 
-            py-2 
-            px-4 
-            border-b-4
-            border-blue-700
-            hover:border-blue-500 
-            rounded inline-block' 
-            type='submit'
-          >
-            Buscar Pelicula
-          </button>
+    <>
+      <div className="container mx-auto flex flex-col justify-center items-center align-tems">
+          <h1 className="text-center text-5xl font-bold text-blue-500 py-9">Finder Film</h1>
+          <form className="w-4/5 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="flex">
+              <input 
+                onChange={handleChange} 
+                value={search} 
+                name="query" 
+                placeholder='Avengers, Star Wars, Matrix...' 
+                className='flex-grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              />
+              <button className='ml-2
+                bg-blue-500 
+                hover:bg-blue-400 
+                text-white 
+                font-bold 
+                py-2 
+                px-4 
+                border-b-4
+                border-blue-700
+                hover:border-blue-500 
+                rounded inline-block' 
+                type='submit'
+              >
+                Buscar Pelicula
+              </button>
+          </div>
+
+
+          <label className='md:w-2/3 block text-gray-500 font-bold pt-3' htmlFor="sort">
+            <input className="mr-2 leading-tight" type="checkbox" id="sort" name="sort" onChange={handleSort} />
+            <span className="text-sm text-white">Ordenar peliculas por nombre</span>
+          </label>
+          </form>       
+        
+
+          {error && <p className='error'>{error}</p>}
+
+          {
+          loading 
+          ? <p className='loading'>Cargando...</p> 
+          :<Movies movies={movies}/>
+          }
+          
+          
       </div>
-
-
-      <label className='md:w-2/3 block text-gray-500 font-bold pt-3' htmlFor="sort">
-        <input className="mr-2 leading-tight" type="checkbox" id="sort" name="sort" onChange={handleSort} />
-        <span className="text-sm">Ordenar peliculas por nombre</span>
-      </label>
-      </form>       
-     
-
-      {error && <p className='error'>{error}</p>}
-      
-
-    <main>  
-      
-      {
-      loading 
-      ? <p className='loading'>Cargando...</p> 
-      :<Movies movies={movies}/>
-      }
-      
-    </main>
-
-  </div>
+  <Footer />
+  </>
   )
 }
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,35 +7,13 @@ function MovieList({ movies, currentPage, moviesPerPage }) {
   const indexOfLastMovie = currentPage * moviesPerPage;
   const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
   const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
-/*  <li className="movie" key={movie.id}>
-      <h3>{movie.title}</h3>
-      <p>{movie.year}</p>
-      <img src={movie.poster} alt={movie.title} />
-    </li> */
+
   return currentMovies.map((movie) => (
    
-    <div key={movie.id}>
-      
-      <div className="bg-white shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8">
-         <div className="h-48 overflow-visible w-1/2">
-            <img src={movie.poster} alt={movie.title} />
-         </div>
-
-         <div className="flex flex-col w-1/2 space-y-4">
-            <div className="flex justify-between items-start">
-              <h2 className="text-3xl font-bold">{movie.title}</h2>
-         <div className="bg-yellow-400 font-bold rounded-xl p-2">7.2</div>
-
-        </div>
-        <div>
-          <div className="text-sm text-gray-400">Año:</div>
-          <div className="text-lg text-gray-800">{movie.year}</div>
-        </div>
-          <p className=" text-gray-400 max-h-40 overflow-y-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div className="flex text-2xl font-bold text-a">$83.90</div>
-      </div>
-      </div>
+    <div className="flex items-center justify-center relative w-full mb-5 group/details" key="{movie.id}">
+        <img src={movie.poster} alt={movie.title} class="w-full rounded-md object-cover" />
     </div>
+   
   ));
 }
 
@@ -55,9 +33,9 @@ export function ListMovies({ movies }) {
 
   return (
     <>
-      <div className="py-3 sm:max-w-xl sm:mx-auto">
+    <div class="columns-2 sm:columns-3 lg:columns-4 gap-5">
         <MovieList movies={movies} currentPage={currentPage} moviesPerPage={moviesPerPage} />
-      </div>
+    </div>
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
