e673227121acf6f2277ba339022104a509f87a0b
Caracolazza <120380236+Caracolazza@users.noreply.github.com>
2023-05-18 13:33:30 -0400

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,22 @@
 // crea un footer en react con estilos css en una constante
 
 const Footer = () => {
-    return (
-        <footer className="footer bg-black text-white py-10">
-          <div className="container mx-auto">
-            <p className="text-center">
-                  Este es el contenido de mi footer. Puedes poner aquí cualquier información que quieras mostrar a tus usuarios.
-            </p>
-            <p className="text-center mt-2">
-                  Hecho con <span className="text-red-600">&hearts;</span> por <a href="https://miweb.com" target="_blank">Mi Web</a>
-            </p>
-            </div>
-        </footer>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <footer className="footer bg-black text-white py-10">
+      <div className="container mx-auto">
+        <p className="text-center">
+          Este es el contenido de mi footer. Puedes poner aquí cualquier
+          información que quieras mostrar a tus usuarios.
+        </p>
+        <p className="text-center mt-2">
+          Hecho con <span className="text-red-600">&hearts;</span> por{" "}
+          <a href="https://www.sebastianlagos.cl" target="_blank">
+            Sebastián Lagos
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
